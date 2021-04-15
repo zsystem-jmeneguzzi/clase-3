1ea1ef5c356e3b18d5b78719543b077b1ccbb4fb
Zsystem <juanmeneguzzi@MBP-de-Juan.fibertel.com.ar>
2021-04-15 00:07:01 -0300

    v2
    v2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-const App = () => {
 
-const syles = {
-  fontSize: 15,
-  color: 'white',
-  backgroundColor:'orange'
-}
+import './App.css';
+// Componentes
+import CardUI from './componentes/cards/cards';
+import Navbar from './componentes/Navbar/Navbar';
+import Usuarios from './componentes/Usuarios/Usuarios'
+import Contador from './componentes/Contador/Contador'
+
+const App = () => {
+  
+
+
 
   return (
   <>
-  <h1>Escribiendo JSX</h1>;
+  <h1>Escribiendo JSX</h1>
+  <Navbar />
+  <div className="Cards-container">
+  <CardUI
+            about="Federico es un musico de la ciudad de Rosario"
+            img="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            name="Federico Romano"
+            date="Usuario activo desde Enero 2019"
 
-  <nav>
-      <ul style={syles}>
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-  </nav>
+          />
+          <CardUI
+            about="Pablo es un ajedrecista profesional de la ciudad de Córdoba"
+            img="https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
+            name="Pablo Rodriguez"
+            date="Usuario activo desde Enero 2021"
+
+          />
+          <CardUI
+            about="Jose es un medico cirujano de la ciudad de Barranquilla"
+            img="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            name="Jose Ramirez"
+            date="Usuario activo desde Enero 2018"
+
+          />
+          <CardUI
+            about="Daniel es un desarrollador frontend de la ciudad de Buenos Aires"
+            img="https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
+            name="Daniel Di Salvo"
+            date="Usuario activo desde Enero 2017"
+          />
+          </div>
+          <div className="Usuarios-container">
+          <Usuarios />
+        </div>
+        <div>
+          <Contador />
+        </div>
 
 </>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// SEMANTIC UI
+import 'semantic-ui-css/semantic.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
